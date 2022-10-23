c358a832f1d731257b4e28b9c812096f08de96f2
ErnestJo <smartlabstech@yahoo.com>
2022-10-23 16:36:21 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './index.css'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 function App() {
   return (
     <div >
-    <h1  className='underline text-3xl'>App</h1>
+   <BrowserRouter>
+   <div className='flex relative dark:bg-main-dark-bg'>
+    <div className='fixed right-4 bottom-4 ' style={{zIndex: '1000'}}>
+<TooltipComponent content='Settings' position='Top'>
+<button >
+<FiSettings />
+</button>
+</TooltipComponent>
+    </div>
+   </div>
+   </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Colorpicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Colorpicker = () => {
+  return (
+    <div>Colorpicker</div>
+  )
+}
+
+export default Colorpicker
