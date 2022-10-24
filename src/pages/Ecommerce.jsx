767626111540e9a885a4d5515b6e75ee3a056038
ErnestJo <smartlabstech@yahoo.com>
2022-10-24 15:49:02 +0300

    fix lay

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -20,8 +20,8 @@ const Ecommerce = () => {
 
   return (
     <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-wrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full  lg:min-w-full-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
